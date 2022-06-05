b681954ff2d4505944d392c7f4d5aeee2d550c7f
reydner14 <100379084+reydner14@users.noreply.github.com>
2022-06-05 15:36:06 -0300

    wrong repo

--- will-it-miss/src/components/Hazard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const YesNo = new Map([
-  [true, 'YES 😱'],
-  [false, 'nope'],
-]);
-
-export default function Hazard({ yes }) {
-  return <span className="hazard">{YesNo.get(yes)}</span>;
-}
